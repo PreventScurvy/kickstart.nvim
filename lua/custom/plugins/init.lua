8cda344d30ac926a70c65946cc9b2a1904475be1
PreventScurvy <bassovnirn@yahoo.com>
2026-06-29 19:23:48 -0500

    Added godot dev

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,4 +1,11 @@
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'Mathijs-Bakker/godotdev.nvim',
+    dependencies = { 'nvim-dap', 'nvim-dap-ui', 'nvim-treesitter' },
+  },
+
+  { 'rcarriga/nvim-dap-ui', dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' } },
+}
